ebd46276e905a9604b698ba0cebdc5f1d9d3ab44
Antonio Zavyalov <antonio.zavyaloff@gmail.com>
2022-11-23 15:50:32 +0600

    Add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def show
+    @user = current_user
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,28 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'static_pages#home'
+
+    authenticated :user do
+      root to: "users#show", as: :authenticated_root
+      get '/user', to: "users#show"
+    end
+
+    unauthenticated do
+      root to: "static_pages#home"
+    end
+
+  devise_scope :user do
+    get '/sign_in', to: 'devise/sessions#new'
+    get '/sign_up', to: 'devise/registrations#new'
+    get '/reset_password', to: 'devise/passwords#new'
+    get '/user', to: 'devise/sessions#new'
+    get '/user/edit', to: 'devise/sessions#new'
+  end
+
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  match '*unmatched', to: 'application#route_not_found', via: :all
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
