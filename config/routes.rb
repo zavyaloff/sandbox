7becfaa59d72275e6cc8f65eb01892ee92696c48
Antonio Zavyalov <antonio.zavyaloff@gmail.com>
2022-11-23 14:17:50 +0600

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # root to: "home#index"
+  devise_for :users
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
